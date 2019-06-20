18cf8c6f076ece013f271d1d422ee5da62e56566
PARKMINJUN <junee0530@naver.com>
2019-06-20 18:07:40 +0900

    second commit of portfolio proj

---
+++ secondproject/urls.py
@@ -0,0 +1,14 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+
+import blog.views
+import portfolio.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', blog.views.home, name='home'),
+    path('blog/',include("blog.urls")),
+    path('portfolio/', portfolio.views.portfolio, name="portfolio"),
+]
